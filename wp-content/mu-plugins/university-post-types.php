a6be680f1490f82aa3e929d13b65658834f83650
Andrew Chung <ascchung@gmail.com>
2025-09-30 21:06:12 -0700

    Initial commit for Fictional University WordPress theme

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,73 @@
+<?php
+function university_post_types()
+{
+    // Event Post Type
+    register_post_type('event', array(
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+        ),
+        'rewrite' => array(
+            'slug' => 'events'
+        ),
+        'public' => true,
+        'menu_icon' => 'dashicons-calendar',
+        'labels' => array(
+            'name' => 'Events',
+            'add_new' => 'Add New Event',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event',
+        )
+    ));
+
+
+    // ProgramPost Type
+    register_post_type('program', array(
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array(
+            'title',
+            'editor',
+        ),
+        'rewrite' => array(
+            'slug' => 'programs'
+        ),
+        'public' => true,
+        'menu_icon' => 'dashicons-awards',
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new' => 'Add New Program',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program',
+        )
+    ));
+
+    // Professor Post Type
+    register_post_type('professor', array(
+        'show_in_rest' => true,
+        'supports' => array(
+            'title',
+            'editor',
+            'thumbnail'
+        ),
+        'public' => true,
+        'menu_icon' => 'dashicons-welcome-learn-more',
+        'labels' => array(
+            'name' => 'Professors',
+            'add_new' => 'Add New Professor',
+            'add_new_item' => 'Add New Professor',
+            'edit_item' => 'Edit Professor',
+            'all_items' => 'All Professors',
+            'singular_name' => 'Professor',
+        )
+    ));
+}
+
+add_action('init', 'university_post_types');
