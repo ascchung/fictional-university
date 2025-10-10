9ad5146f584b01527ec72d94a0b4dbe199987344
Andrew Chung <ascchung@gmail.com>
2025-10-09 17:45:07 -0700

    custom route added

---
+++ .php-cs-fixer.php
@@ -0,0 +1,13 @@
+<?php
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+        'blank_line_after_opening_tag' => true,
+        'no_blank_lines_after_phpdoc' => true,
+        'single_quote' => true,
+        'no_extra_blank_lines' => true,
+    ]);
