6188fbc5abec53ec3e8b98674f4186ef3abfb91b
Andrew Chung <ascchung@gmail.com>
2025-10-10 01:09:57 -0700

    pulled campuses in relation to programs on live search"

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -48,6 +48,17 @@ function universitySearchResults($data)
         }
 
         if (get_post_type() == 'program') {
+            $relatedCampuses = get_field('related_campus');
+
+            if ($relatedCampuses) {
+                foreach ($relatedCampuses as $campus) {
+                    array_push($results['campuses'], [
+                        'title' => get_the_title($campus),
+                        'url' => get_the_permalink($campus)
+                    ]);
+                }
+            }
+
             array_push($results['programs'], [
                 'title' => get_the_title(),
                 'url' => get_the_permalink(),
@@ -94,13 +105,32 @@ function universitySearchResults($data)
         }
 
         $programRelationshipQuery = new WP_Query([
-            'post_type' => 'professor',
+            'post_type' => [
+                'professor', 'event'
+            ],
             'meta_query' => $programsMetaQuery
    ]);
 
         while ($programRelationshipQuery->have_posts()) {
             $programRelationshipQuery->the_post();
 
+            if (get_post_type() == 'event') {
+                $eventDate = new DateTime(get_field('event_date'));
+                $description = null;
+                if (has_excerpt()) {
+                    $description = get_the_excerpt();
+                } else {
+                    $description =  wp_trim_words(get_the_excerpt(), 18);
+                }
+                array_push($results['events'], [
+                    'title' => get_the_title(),
+                    'url' => get_the_permalink(),
+                    'month' => $eventDate->format('M'),
+                    'day' => $eventDate->format('d'),
+                    'description' => $description,
+                    'id' => get_the_ID()
+                ]);
+            }
             if (get_post_type() == 'professor') {
                 array_push($results['professors'], [
                     'title' => get_the_title(),
@@ -109,9 +139,11 @@ function universitySearchResults($data)
                     'id' => get_the_ID()
                 ]);
             }
+
         }
 
         $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+        $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
 
     }
 
