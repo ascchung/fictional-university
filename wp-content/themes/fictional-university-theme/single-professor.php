4fe0dd93e90350d84519860c94fc5ba13c4549db
Andrew Chung <ascchung@gmail.com>
2025-10-27 16:03:47 -0700

    professor like feature"

--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -5,7 +5,7 @@ get_header();
 while (have_posts()) {
     the_post();
     pageBanner();
-?>
+    ?>
 
 
     <div class="container container--narrow page-section">
@@ -15,6 +15,40 @@ while (have_posts()) {
                     <?php the_post_thumbnail('professorPortrait'); ?>
                 </div>
                 <div class="two-thirds">
+                    <?php
+    $likeCount = new WP_Query([
+        'post_type' => 'like',
+        'meta_query' => [
+            [
+                'key' => 'liked_professor_id',
+                'compare' => '=',
+                'value' => get_the_ID()
+            ]
+        ]
+    ]);
+    $existStatus = 'no';
+
+    $existQuery = new WP_Query([
+        'author' => get_current_user_id(),
+        'post_type' => 'like',
+        'meta_query' => [
+            [
+                'key' => 'liked_professor_id',
+                'compare' => '=',
+                'value' => get_the_ID()
+            ]
+        ]
+    ]);
+    if ($existQuery->found_posts) {
+        $existStatus = 'yes';
+    }
+    ?>
+
+                    <span class="like-box" data-exists="<?php echo $existStatus;?>">
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>
+                        <i class="fa fa-heart" aria-hidden="true"></i>
+                        <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
+                    </span>
                     <?php the_content(); ?>
                 </div>
             </div>
@@ -22,21 +56,21 @@ while (have_posts()) {
         </div>
 
         <?php
-        $subjectsTaught = get_field('related_programs');
+            $subjectsTaught = get_field('related_programs');
 
-        if ($subjectsTaught) {
-            echo '<hr class="section-break">';
-            echo '<h2 class="headline headline--medium">Subject(s) Taught)</h2>';
-            echo '<ul class="link-list min-list">';
-            foreach ($subjectsTaught as $program) { ?>
+    if ($subjectsTaught) {
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Subject(s) Taught)</h2>';
+        echo '<ul class="link-list min-list">';
+        foreach ($subjectsTaught as $program) { ?>
                 <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
         <?php }
-            echo '</ul>';
-        }
+        echo '</ul>';
+    }
 
-        ?>
+    ?>
 
     <?php }
 
 get_footer();
-    ?>
+?>
